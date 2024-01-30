815c6e990529d84e516ec4609b88d5d1bfb128b0
RyanMYang <32602114+ryanmyang@users.noreply.github.com>
2024-01-29 21:29:22 -0800

    Added root mean sqre prints per target. Not 100% on correctness

--- motor_learning.py
+++ motor_learning.py
@@ -3,20 +3,14 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from enum import Enum
 import re
+import math
+from math_helper import normalize_vectors, pnt2line, distance
 
 class plane(Enum):
     XY = "Frontal"
     YZ = "YZ"
     XZ = "Horizontal"
 
-def normalize_vectors(vectors, radius):
-    # Calculate the current magnitudes (lengths) of each vector
-    magnitudes = np.linalg.norm(vectors, axis=1)
-
-    # Normalize vectors to the specified radius
-    normalized_vectors = vectors * (radius / magnitudes)[:, np.newaxis]
-
-    return normalized_vectors
 
 class motor_learning:
     targets = np.array([[0.0,1.0], [1.0,1.0], [1.0, 0.0], [1.0, -1.0], [0.0,-1.0], [-1.0, -1.0], [-1.0, 0.0], [-1.0, 1.0]])
@@ -118,9 +112,7 @@ class motor_learning:
         pass
         
     def plot_2d(self):
-        
-
-        # num_ticks = 10
+        # Adjust 2D portions and flipping based on plane used for data collection
         x = 0
         y = 2
         x_adjust = 1
@@ -151,41 +143,65 @@ class motor_learning:
         
         # print(x_arr)
         # print(y_arr)
-        
+        # Initialize Plot 
         ax = self.fig.add_subplot(111)
         ax.set_aspect('equal', adjustable='box')
+        ax.margins(x=0.1,y=0.1)
+        ax.set_title("", y=1.05, fontsize=18)
+        ax.set_title(f"Position of arm for each target\n{self.arm} arm, {self.plane.value} plane", fontsize=10)
 
+        # Plot targets
         for i in range(len(self.targets)):
             ax.plot([0, self.targets[i][0]], [0,self.targets[i][1]], marker='o', linestyle='-', color='black')
             ax.annotate(str(i+1), (self.targets[i][0],self.targets[i][1]), textcoords='offset points', xytext= (-3,-4) + (self.targets[i])*30)
-        ax.margins(x=0.1,y=0.1)
 
+        # Split positions into target sections based on target times
         split_indices = [int(i[2]) for i in self.target_time_index_array]
         split_indices.remove(0)
         split_positions = np.split(self.positions, split_indices)
-        print("SPLIT INDICES")
-        print(split_indices)
-        print(len(self.positions))
 
-        print("SPLIT POSITIONS")
-        print(len(split_positions))
+        # print("SPLIT INDICES")
+        # print(split_indices)
+        # print(len(self.positions))
+        # print("SPLIT POSITIONS")
+        # print(len(split_positions))
+        # print(split_positions)
 
-        print(split_positions)
+        # Plot all positions by iterating through the split positions
         for i in range(len(self.target_set_times)):
+            curr_target = self.target_set_times[i][0]
             x_arr = np.array([(p[x] * x_adjust) for p in split_positions[i]])
             y_arr = np.array([(p[y] * y_adjust) for p in split_positions[i]])
-            ax.plot(x_arr, y_arr, marker='.', linestyle='-', label=self.target_set_times[i][0])
+            ax.plot(x_arr, y_arr, marker='.', linestyle='-', label=curr_target)
+
+            print("Target" + str(curr_target))
+            print(list(self.targets[curr_target-1]) +[0])
+
+            # Calculate distances
+            target_distances = []
+            for j in range(len(x_arr)):
+                distance = pnt2line([x_arr[j],y_arr[j],0], [0,0,0], list(self.targets[curr_target-1]) +[0])[0]
+                target_distances.append(distance)
+            # print(f"target distances : {target_distances}")
+
+            distances_squared = [(d ** 2) for d in target_distances]
+            # print(f"Distances squared: {distances_squared}")
+            print(f"Max in distance square sum: {max(distances_squared)}")
+            distance_square_sum = sum(distances_squared)
+            print(f"Distance square sum: {distance_square_sum}")
+            print(f"Split positions size for target: {len(split_positions[i])}")
+            rmsd = math.sqrt(distance_square_sum/len(target_distances))
+            print(f"The RMSD for target {curr_target} is {rmsd}")
+            print()
+            
+
         ax.legend(bbox_to_anchor=(1.2, 1))
-        ax.set_title("", y=1.05, fontsize=18)
-        ax.set_title(f"Position of arm for each target\n{self.arm} arm, {self.plane.value} plane", fontsize=10)     
-
-
-
-        
+        plt.show()
+             
 
 
 if __name__ == "__main__":
-    ml = motor_learning("MotorLearning_01_29_2024_19_21_21_Left_YZ_normal.txt")
+    ml = motor_learning("MotorLearning_01_29_2024_20_59_28_Right_XZ_normal.txt")
     ml.plot_2d()
-    plt.show()
+    
     
