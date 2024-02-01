404221db94f32ecfab073c397467c5317b69de4b
RyanMYang <32602114+ryanmyang@users.noreply.github.com>
2024-01-31 19:29:38 -0800

    Added average velocity calculation

--- motor_learning.py
+++ motor_learning.py
@@ -40,11 +40,6 @@ class motor_learning:
     
             # Process the first line differently
             self.process_first_line(first_line)
-            # print(self.arm)
-            # print(self.shoulder_pos)
-            # print(self.plane)
-            # print(self.game_radius)
-
             # Process the remaining lines, including last line
             last_target = -1
             for line in f:
@@ -71,27 +66,43 @@ class motor_learning:
                     # If diff, record that and change
                     self.speed_set_times.append([speed, time])
             
-            # print(np.shape(self.times))
-            # print(np.shape(self.target_set_times))
-            target_set_times_times_only = [couple[1] for couple in self.target_set_times]
-            # print(f"TSTTO: {target_set_times_times_only}")
-            splitIndices = [np.searchsorted(self.times, t) for t in target_set_times_times_only]
-            # print("SPLIT TIMES")
-            # print([self.times[t] for t in splitIndices])
-            # print(splitIndices)
-            self.target_time_index_array = np.column_stack((self.target_set_times, splitIndices))
-            # print()
-            # print("target, time, index")
-            # print(str(target_time_index))
+        target_set_times_times_only = [couple[1] for couple in self.target_set_times]
+        # print(f"TSTTO: {target_set_times_times_only}")
+        splitIndices = [np.searchsorted(self.times, t) for t in target_set_times_times_only]
+        
+        self.target_time_index_array = np.column_stack((self.target_set_times, splitIndices))
+        
+        self.targets = normalize_vectors(motor_learning.targets, self.game_radius)
+        # print(f"TARGETS: {self.targets}")    
 
-            # print("done")
-            # print("TIMES: " + str(self.times))
-            # print(f"POSITIONS: {self.positions}")
-            # print(f"TARGET SET TIMES: {self.target_set_times}")
-            # print(f"SPEED SET TIMES: {self.speed_set_times}")
-            self.targets = normalize_vectors(motor_learning.targets, self.game_radius)
-            # print(f"TARGETS: {self.targets}")
-            self.fig = plt.figure(figsize=(10, 6), dpi=80)
+        self.x = 0
+        self.y = 2
+        self.x_adjust = 1
+        self.y_adjust = 1
+        match self.plane:
+            case plane.XZ:
+                self.x = 0
+                self.y = 2
+                self.x_adjust = -1
+                self.y_adjust = -1
+            case plane.XY:
+                self.x = 0
+                self.y = 1
+                self.x_adjust = -1
+                self.y_adjust = -1
+            case plane.YZ:
+                if self.arm == 'Right':
+                    self.x = 2
+                    self.y = 1
+                    self.x_adjust = 1
+                    self.y_adjust = -1
+                if self.arm == 'Left':
+                    self.x = 2
+                    self.y = 1
+                    self.x_adjust = -1
+                    self.y_adjust = -1
+        
+        self.fig = plt.figure(figsize=(10, 6), dpi=80)
 
 
     def process_first_line(self, line: str):
@@ -118,44 +129,25 @@ class motor_learning:
             
     def process_last_line(self, words: list[str]):
         pass
-        
-    def plot_2d(self):
-        # Adjust 2D portions and flipping based on plane used for data collection
-        x = 0
-        y = 2
-        x_adjust = 1
-        y_adjust = 1
-        match self.plane:
-            case plane.XZ:
-                x = 0
-                y = 2
-                x_adjust = -1
-                y_adjust = -1
-            case plane.XY:
-                x = 0
-                y = 1
-                x_adjust = -1
-                y_adjust = -1
-            case plane.YZ:
-                if self.arm == 'Right':
-                    x = 2
-                    y = 1
-                    x_adjust = 1
-                    y_adjust = -1
-                if self.arm == 'Left':
-                    x = 2
-                    y = 1
-                    x_adjust = -1
-                    y_adjust = -1
+
+    def calculate_average_velocity(self) -> int:
+        # 1. Store all the differences
+        position_diffs = np.diff(self.positions, axis=0)
+        # 2. Find all the distances
+        euclidean_distances = np.linalg.norm(position_diffs, axis=1)
+        # 3. Sum to get the distance, and divide by time diff in secs
+        time_diff = self.times[-1] - self.times[0]
+        return sum(euclidean_distances)/time_diff
 
         
-        # print(x_arr)
-        # print(y_arr)
+    def plot_2d(self) -> None:
+        # Adjust 2D portions and flipping based on plane used for data collection
+        
         # Initialize Plot 
         ax = self.fig.add_subplot(111)
         ax.set_aspect('equal', adjustable='box')
         ax.margins(x=0.1,y=0.1)
-        ax.set_title(f"Position of arm for each target\n{self.arm} arm, {self.plane.value} plane", fontsize=10)
+        ax.set_title(f"Position of arm for each target\n{self.arm} arm, {self.plane.value} plane, {self.speed_set_times[0][0]} speed", fontsize=10)
 
         # Plot targets
         for i in range(len(self.targets)):
@@ -177,8 +169,8 @@ class motor_learning:
         # Plot all positions by iterating through the split positions
         for i in range(len(self.target_set_times)):
             curr_target = self.target_set_times[i][0]
-            x_arr = np.array([(p[x] * x_adjust) for p in split_positions[i]])
-            y_arr = np.array([(p[y] * y_adjust) for p in split_positions[i]])
+            x_arr = np.array([(p[self.x] * self.x_adjust) for p in split_positions[i]])
+            y_arr = np.array([(p[self.y] * self.y_adjust) for p in split_positions[i]])
             ax.plot(x_arr, y_arr, marker='.', linestyle='-', label=curr_target)
 
             # print("Target" + str(curr_target))
@@ -199,7 +191,7 @@ class motor_learning:
             # print(f"Split positions size for target: {len(split_positions[i])}")
             rmsd = math.sqrt(distance_square_sum/len(target_distances))
             RMSD_caption += f"\nTarget {curr_target}: {'{:.3f}'.format(rmsd)}"
-            print(f"The RMSD for target {curr_target} is {rmsd}")
+            # print(f"The RMSD for target {curr_target} is {rmsd}")
             
         ax.text(-0.45, 0, RMSD_caption, wrap=True, horizontalalignment='right', fontsize=8)
         ax.legend(bbox_to_anchor=(1.2, 1))
@@ -208,5 +200,8 @@ class motor_learning:
 
 
 if __name__ == "__main__":
-    ml = motor_learning("MotorLearning_01_31_2024_17_41_34_Left_XZ_fast.txt")
+    ml = motor_learning("MotorLearning_01_31_2024_17_35_45_Left_XZ_normal.txt")
+    print(f"AVERAGE VELOCITY: {ml.calculate_average_velocity()}")
+
+    
     ml.plot_2d()
